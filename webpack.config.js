c4389f3916f4e6a836839b6a08a8370da331607c
cherrih <cherri.hartigan@gmail.com>
2019-04-05 15:15:23 -0700

    Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
     filename: 'bundle.js',
@@ -14,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           }
         },
       }
