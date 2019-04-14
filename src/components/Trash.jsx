78461c6e9866f565e967e3ee2eb79e016c1b8c9d
cherrih <cherri.hartigan@gmail.com>
2019-04-13 18:17:15 -0700

    Render floating trash

--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Engine, Render, World, Bodies, Mouse, MouseConstraint, Events } from 'matter-js';
+import Matter from 'matter-js';
+import MatterWrap from 'matter-wrap';
+import { Engine, Render, World, Bodies, Mouse, MouseConstraint, Events, Composites } from 'matter-js';
 
 class Trash extends React.Component {
   constructor(props) {
@@ -9,30 +11,103 @@ class Trash extends React.Component {
       h: window.innerHeight
     };
   }
+  
   componentDidMount () {
-
-    var engine = Engine.create();
-    var render = Render.create({
-        element: this.refs.trash,
-        engine: engine,
-        options: {
-          width: this.state.w,
-          height: this.state.h,
-          background: 'transparent',
-          wireframes: false
-        }
+    const {w, h} = this.state;
+    Matter.use(MatterWrap);
+    const reference = this.refs.trash
+    const engine = Engine.create();
+    engine.world.gravity.y = 0.5;
+    const render = Render.create({
+      element: reference,
+      engine: engine,
+      options: {
+        width: w,
+        height: h,
+        background: 'transparent',
+        wireframes: false
+      }
     });
-    var ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
-    var ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
-    // World.add(engine.world, [
-    //   // walls
-    //   Bodies.rectangle(200, 0, 600, 50, { isStatic: true }),
-    //   Bodies.rectangle(200, 600, 600, 50, { isStatic: true }),
-    //   Bodies.rectangle(260, 300, 50, 600, { isStatic: true }),
-    //   Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-    // ]);
-    World.add(engine.world, [ballA, ballB]);
-    var mouse = Mouse.create(render.canvas),
+    
+    Render.run(render);
+    
+    const steak = Bodies.rectangle(w/2, 0, 100, 100, {
+      render: {
+        sprite: {
+          texture: '../../images/steak.png',
+          xScale: 0.4,
+          yScale: 0.4,
+          restitution: 1,
+          // density: 0.04,
+          // friction: 0.01,
+          // frictionAir: 0.00001,
+        }
+      }
+    })
+    const tampon = Bodies.rectangle(w/2, 0, 100, 100, {
+      render: {
+        sprite: {
+          texture: '../../images/tampon.png',
+          xScale: 0.4,
+          yScale: 0.4,
+          restitution: 1,
+          
+          inertia: Infinity,
+        }
+      }
+    })
+    const bag = Bodies.rectangle(w/2, 0, 100, 100, {
+      render: {
+        sprite: {
+          texture: '../../images/thankyoubag.png',
+          xScale: 0.4,
+          yScale: 0.4,
+          restitution: 1,
+          density: 1,
+          friction: 0.01,
+          frictionAir: 0.00001,
+          inertia: Infinity
+        }
+      }
+    })
+
+    // const createShape = function (x,y) {
+    //   const images = ['steak', 'tampon', 'thankyoubag'];
+    //   return Bodies.rectangle(x, y, 100, 100, {
+    //     render: {
+    //       sprite: {
+    //         texture: `images/${images[i]}.png`,
+    //         xScale: 0.5,
+    //         yScale: 0.5
+    //       }
+    //     },
+    //     plugin: {
+    //       wrap: {
+    //         min: { x: 0, y: 0 },
+    //         max: { x: w, y: h }
+    //       }
+    //     }
+    //   })
+    // }
+    const wallOptions = {
+      isStatic: true,
+      render: {
+        visible: false,
+        restitution: 1,
+        inertia: Infinity
+      }
+    }
+
+    const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, wallOptions)
+    const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions)
+    const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions)
+    const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 100, wallOptions)
+
+    // const shapes = Composites.stack(50, 50, 10, 10, 40, 40, function (x, y) {
+    //   return createShape(x, y)
+    // })
+
+    const mouse = Mouse.create(render.canvas),
       mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
         constraint: {
@@ -42,15 +117,33 @@ class Trash extends React.Component {
           }
         }
       });
+    // const mouse = MouseConstraint.create(engine, {
+    //   element: reference,
+    //   constraint: {
+    //     render: {
+    //       visible: false
+    //     }
+    //    }
+    //  })
+
+    World.add(engine.world, [
+      steak,
+      ground,
+      ceiling,
+      leftWall,
+      rightWall,
+      mouse,
+      tampon,
+      bag
+    ])
     World.add(engine.world, mouseConstraint);
 
-    Events.on(mouseConstraint, "mousedown", function(event) {
-      World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
-    });
+    // Events.on(mouseConstraint, "mousedown", function(event) {
+    //   // World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
+    //   console.log('mouse')
+    // });
 
     Engine.run(engine);
-
-    Render.run(render);
   }
 
   render () {
